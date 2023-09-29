aa4e1edb859279814a5a0f395f9ea11318969ccf
DennAmo <dennisaubryy@gmail.com>
2023-09-29 13:14:51 +0200

    ok

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Votre code JavaScript à exécuter lorsque le document est prêt
     console.log("Le document est prêt !");
 
-
-
-
+    console.log(document.getElementById("jambon"))
 
 });
 
