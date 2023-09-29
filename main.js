dbba1d5950020bf89e1021217245cd46c20de8a2
DennAmo <dennisaubryy@gmail.com>
2023-09-29 13:11:53 +0200

    starting

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Votre code JavaScript à exécuter lorsque le document est prêt
+    console.log("Le document est prêt !");
+
+
+
+
+
+});
+
